c32a8170f7aa459aaf95aabda6581bcca4ae2c60
SheilaRocha <sheilarreis@gmail.com>
2022-05-03 17:32:10 -0300

    Organizando arquivos

---
+++ sistemasDistribuidos/abril/05abril/flaskroutetutorial/ex1.py
@@ -0,0 +1,11 @@
+# Baseado no tutorial disponivel em: https://pythonbasics.org/flask-tutorial-templates/
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+@app.route('/')
+@app.route('/index')
+def index():
+    users = [ 'Rosalia','Adrianna','Victoria' ]
+    return render_template('index.html', title='Welcome', members=users)
+app.run(host='0.0.0.0', port=81)
